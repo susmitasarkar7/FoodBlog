e120d91e90df04890be0cbed5a59492a0fcfe93b
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-05-29 18:38:04 +0530

    ignore

--- wp-content/themes/foodblog/template-parts/content-hero.php
+++ wp-content/themes/foodblog/template-parts/content-hero.php
@@ -8,9 +8,9 @@
 			<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
 
 			<div class="hero-slide-item set-bg" data-setbg="
-			<?php echo get_theme_mod('showcase_image', get_bloginfo('template_url').'/assets/img/slider-bg-1.jpg'); ?>
-
-			">			<!-- <?php //echo get_field('hero_slider_image')['url']; ?> -->
+			<?php echo get_field('hero_slider_image')['url']; ?>
+			">		
+			<!-- <?php //echo get_theme_mod('showcase_image', get_bloginfo('template_url').'/assets/img/slider-bg-1.jpg'); ?>	 -->
 			<div class="hs-text">
 				<!-- <h2 class="hs-title-1"><span><?php echo get_theme_mod('showcase_heading_1', 'Healthy Recipes'); ?></span></h2> -->
 				<h2 class="hs-title-1"><span><?php the_field('title_1'); ?></span></h2>
