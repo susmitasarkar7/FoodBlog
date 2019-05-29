03b7b66589a642cad61d9dbd58b874b0ee4ddc40
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-05-29 13:29:46 +0530

    login register modified

--- wp-content/themes/foodblog/functions.php
+++ wp-content/themes/foodblog/functions.php
@@ -265,11 +265,11 @@ echo '<p>Welcome to your site! Need help? Contact me <a href="mailto:youremail@y
 
 //remove menus from dashboard
 function remove_menus(){
-	remove_menu_page( 'themes.php' );          // Appearance
-	remove_menu_page( 'plugins.php' );         // Plugins
+	// remove_menu_page( 'themes.php' );          // Appearance
+	// remove_menu_page( 'plugins.php' );         // Plugins
 	remove_menu_page( 'users.php' );           // Users
 	remove_menu_page( 'tools.php' );           // Tools
-	remove_menu_page( 'options-general.php' ); // Settings
+	// remove_menu_page( 'options-general.php' ); // Settings
 }
 add_action( 'admin_menu', 'remove_menus' );
 
